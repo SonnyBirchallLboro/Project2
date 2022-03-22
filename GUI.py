5ddb8bf1c8c7946c9d9b74db8bd175ba2fc6fd5c
Sonny Birchall <sonny@davidbirchall.com>
2022-03-22 10:33:08 +0000

    Changed Classes into another file.

--- GUI.py
+++ GUI.py
@@ -1,39 +1,12 @@
 
 import sys
-import numpy
+import numpy   #unneeded rn
 import pygame
 from pygame.locals import *
 
-
-class Obstacle(pygame.sprite.Sprite):
-    def __init(self, type, location):
-        super().__init__()
-        self.type = type
-        self.x = location[0]
-        self.y = location[1]
-        if self.type == 'Person':
-            self.width = 0.4
-            self.height = 1.8
-        #    self.image = pygame.image.load(r"Images\Person.jpg").convert()    #may need to change args
-        elif self.type == 'Car':
-            self.width = 2
-            self.height = 1.5
-        #    self.image = pygame.image.load(r"Images\Car.jpg").convert()     #may need to change args
-        self.image = pygame.image.load(r"Images\%s.jpg" % type).convert()
-        self.rect = self.image.get_rect(topleft=(self.x-(self.width/2), self.y-(self.height/2)))
+from Sprites.py import Obstacles, Bus
 
 
-class Bus(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load(r"Images\bus.jpg").convert()
-        self.height = 3 * 50
-        self.width = 2.5 * 50 
-        self.x = 350
-        self.y = 400
-        self.image = pygame.transform.scale(self.image, (self.width, self.height))
-        # self.rect = self.image.get_rect(topleft=(self.x-(self.width/2), self.y-(self.height/2)))
-        self.rect = self.image.get_rect(center=(self.x, self.y))
 
         
 
@@ -41,9 +14,9 @@ class Bus(pygame.sprite.Sprite):
 class GUI:
     def __init__(self):
         pygame.init()
-        self.fps = 60    
+        self.fps = 24    
         self.fpsClock = pygame.time.Clock()
-        WIDTH, HEIGHT = 700, 800
+        WIDTH, HEIGHT = 640, 640
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Bus surroundings GUI")
         self.screen.fill((255,255,255))
@@ -52,8 +25,6 @@ class GUI:
         self.all_sprites.add(self.bus)
         self.all_sprites.draw(self.screen)
 
-        
-
 
     def run(self):
         while True:
@@ -61,12 +32,13 @@ class GUI:
                 if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
-            
 
+            #update display
             pygame.display.update()
             self.all_sprites.update()
 
-        self.fpsClock.tick(self.fps)
+            self.fpsClock.tick(self.fps)
+
 
 
 
---
+++ Sprites.py
@@ -0,0 +1,36 @@
+
+import sys, pygame
+
+
+
+class Bus(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load(r"Images\bus.jpg").convert()
+        self.height = 3 * 50
+        self.width = 2.5 * 50 
+        self.x = 350
+        self.y = 400
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        # self.rect = self.image.get_rect(topleft=(self.x-(self.width/2), self.y-(self.height/2)))
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+
+
+
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init(self, type, location):
+        super().__init__()
+        self.type = type
+        self.x = location[0]
+        self.y = location[1]
+        if self.type == 'Person':
+            self.width = 0.4
+            self.height = 1.8
+        #    self.image = pygame.image.load(r"Images\Person.jpg").convert()    #may need to change args
+        elif self.type == 'Car':
+            self.width = 2
+            self.height = 1.5
+        #    self.image = pygame.image.load(r"Images\Car.jpg").convert()     #may need to change args
+        self.image = pygame.image.load(r"Images\%s.jpg" % type).convert()
+        self.rect = self.image.get_rect(topleft=(self.x-(self.width/2), self.y-(self.height/2)))
